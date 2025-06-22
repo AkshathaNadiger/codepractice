341b4f7ab084828f1df6cf28d11849aa1f33e80e
Akshatha <akshathanadiger20@gmail.com>
2025-06-22 16:35:57 +0530

    practice

---
+++ JavaPractice/ReverseAnArray.java
@@ -0,0 +1,22 @@
+public class ReverseAnArray {
+
+    public static void main(String args[]){
+        int[] arr = {10,20,30,40,50};
+        int start =0;
+        int end = arr.length-1;
+        int temp;
+
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        System.out.println("Reversed array is: ");
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
